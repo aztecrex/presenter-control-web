e7d8c63e2a6e814b6f884af50d3004cd4296cc20
Greg Wiley <aztec.rex@jammm.com>
2017-09-24 18:04:46 -0700

    some styling

--- src/ui/View.purs
+++ src/ui/View.purs
@@ -2,7 +2,8 @@ module UI.View (view) where
 
 import Prelude (const, discard, show, ($), (<>))
 import Data.Maybe (maybe)
-import Text.Smolder.Markup (text, (#!))
+import Text.Smolder.Markup (text, (#!), (!))
+import Text.Smolder.HTML.Attributes (className, style)
 import Data.Foldable (for_)
 import Data.Lens ((^.))
 import Text.Smolder.HTML (div, p, button, br)
@@ -13,15 +14,15 @@ import UI.Event(Event(..))
 
 pbutton :: String -> HTML Event
 pbutton url = do
-    button #! onClick (const (Location url)) $ text url
+    button ! className "btn" #! onClick (const (Location url)) $ text url
     br
 
 view :: State -> HTML Event
 view state = do
     div $ do
         div $ do
-            button #! onClick (const Previous) $ text "Previous"
-            button #! onClick (const Next) $ text "Next"
-            button #! onClick (const Restart) $ text "Restart"
-        div $ do
+            button ! className "btn-lg" #! onClick (const Previous) $ text "Previous"
+            button ! className "btn-lg" #! onClick (const Next) $ text "Next"
+            button ! className "btn-lg" #! onClick (const Restart) $ text "Restart"
+        div ! style "padding-top: 2em;" $ do
             for_ (state ^. presentations) pbutton
