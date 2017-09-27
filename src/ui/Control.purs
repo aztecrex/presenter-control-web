4c4fda3c712dad2cf4874435a797c5136b8f38dc
Greg Wiley <aztec.rex@jammm.com>
2017-09-26 20:01:01 -0700

    restart slides on presentation change

--- src/ui/Control.purs
+++ src/ui/Control.purs
@@ -11,10 +11,10 @@ reduce :: Event -> State -> State
 reduce Next s = s # page +~ 1
 reduce Previous s = s # page -~ 1
 reduce Restart s = s # page .~ 1
-reduce (Location loc) s = s # url .~ loc
+reduce (Location loc) s = s # url .~ loc # page .~ 1
 reduce (PresentationInputChange value) s = s # presentationInput .~ value
 reduce AddPresentation s = (clearInput <<< appendLocation) s
-  where clearInput s = s # presentationInput .~ ""
-        appendLocation s = s # presentations <>~ [s ^. presentationInput ]
+  where clearInput s' = s' # presentationInput .~ ""
+        appendLocation s' = s' # presentations <>~ [s' ^. presentationInput ]
 reduce (Presentations ps) s = s # presentations .~ ps
 reduce _ s = s
